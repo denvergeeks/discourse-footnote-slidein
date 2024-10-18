2253d88582d19ca9ec3d10a46a9a10fe4dc05500
denvergeeks <jen@denvergeeks.com>
2024-10-18 15:55:31 -0600

    Update init-footnote-panel.js

--- javascripts/discourse/initializers/init-footnote-panel.js
+++ javascripts/discourse/initializers/init-footnote-panel.js
@@ -1,19 +1,20 @@
-import Component from "@ember/component";
-import { action } from "@ember/object";
+import { withPluginApi } from "discourse/lib/plugin-api";
 
-export default Component.extend({
-  classNames: ["footnote-panel"],
-  isVisible: false,
-  content: "",
-
-  @action
-  togglePanel() {
-    this.toggleProperty("isVisible");
-  },
-
-  @action
-  setContent(content) {
-    this.set("content", content);
-    this.set("isVisible", true);
+export default {
+  name: "init-footnote-panel",
+  initialize() {
+    withPluginApi("0.8.31", api => {
+      api.decorateCooked($elem => {
+        $elem.find("sup.footnote").each((i, el) => {
+          const $footnote = $(el);
+          const content = $footnote.attr("title");
+          $footnote.removeAttr("title");
+          $footnote.on("click", () => {
+            const panel = api.container.lookup("component:footnote-panel");
+            panel.setContent(content);
+          });
+        });
+      });
+    });
   }
-});
+};
